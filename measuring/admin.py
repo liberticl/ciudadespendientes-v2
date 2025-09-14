469eab0b0f27912d30cfc8127e012b39981089da
Francisco Vergara <fvergara@drivetech.pro>
2025-09-14 18:58:22 -0300

    feat(measuring): se agrega sitio admin

--- measuring/admin.py
+++ measuring/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+@admin.register(models.Device)
+class DeviceAdmin(admin.ModelAdmin):
+    """
+        Sitio administrativo para Device
+    """
+    list_display = ('device', 'token')
+    list_filter = ('device', 'token',)
+    search_fields = ('device', 'token', 'coords')
+    readonly_fields = ('token',)
+    fieldsets = (
+        ('General', {
+            'fields': (
+                'device', 'token', 'coords',)}),
+    )
+    # filter_horizontal = ('sectors',)
+
+
+@admin.register(models.TrafficCount)
+class TrafficCountAdmin(admin.ModelAdmin):
+    """
+        Sitio administrativo para Device
+    """
+    list_display = ('device', 'datetime')
+    list_filter = ('device',)
+    search_fields = ('device',)
+    fieldsets = (
+        ('General', {
+            'fields': (
+                'device', 'datetime')}),
+        ('Conteo', {
+            'fields': (
+                'car_count', 'person_count', 'bicycle_count',
+                'motorcycle_count', 'truck_count', 'bus_count',)}),
+    )
+    # filter_horizontal = ('sectors',)
