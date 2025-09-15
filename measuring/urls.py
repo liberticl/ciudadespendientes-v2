8edeeeff324d2e6e63d221425400300daf9835c6
Francisco Vergara <fvergara@drivetech.pro>
2025-09-14 22:46:25 -0300

    fix(measuring): se corrige url para endpoint

--- measuring/urls.py
+++ measuring/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import TrafficCountAPIView
 
 urlpatterns = [
-    path('traffic/', TrafficCountAPIView.as_view(), name='traffic-api'),
+    path('trafico/', TrafficCountAPIView.as_view(), name='traffic-api'),
 ]
